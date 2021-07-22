97479658345f36970ec38a763c5472a1370f8631
Lukas Keel <lrk83@cornell.edu>
2021-07-22 16:01:40 -0400

    node requests for candidates

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+//Express middleware
 app.use(express.urlencoded({ extrended: false}));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'MySQLy76y76y76',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
@@ -15,6 +29,10 @@ app.get('/', (req,res) =>{
         messgae: 'Hello World'
     });
 });
+
+db.query(`SELECT * FROM candidates`, (err,rows) =>{
+    console.log(rows);
+})
 
 app.use((req,res) =>{
     res.status(404).end();
