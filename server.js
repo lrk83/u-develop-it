b578d612792e1044355e3d7af5fce82c7fc17a5a
Lukas Keel <lrk83@cornell.edu>
2021-07-22 16:21:02 -0400

    query routes for candidates (currently commented out)

--- server.js
+++ server.js
@@ -30,9 +30,36 @@ app.get('/', (req,res) =>{
     });
 });
 
-db.query(`SELECT * FROM candidates`, (err,rows) =>{
-    console.log(rows);
-})
+//db.query(`SELECT * FROM candidates`, (err,rows) =>{
+    //console.log(rows);
+//});
+
+//GET a single candidate
+//db.query(`SELECT * FROM candidates WHERE ID = 1`, (err,row) => {
+  //  if (err){
+    //    console.log(err);
+    //}
+    //console.log(row);
+//});
+
+//db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) =>{
+    //if (err){
+      //  console.log(err);
+    //}
+    //console.log(result);
+//});
+
+//CREATE a candidate
+//const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+  //              VALUES (?,?,?,?)`;
+//const params = [1, 'Ronald', 'Firbank', 1];
+
+//db.query(sql, params, (err,result) => {
+    //if (err){
+    //    console.log(err);
+   // }
+    //console.log(result);
+//});
 
 app.use((req,res) =>{
     res.status(404).end();
